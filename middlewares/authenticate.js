f1395b891b633c2191e571243616e73ee6bc3161
yagya <yagyagoel87@gmail.com>
2024-01-11 03:59:11 +0530

    created a authentication middleware where it checks whether the person is a valid user

---
+++ middlewares/authenticate.js
@@ -0,0 +1,24 @@
+const {jwtsecretkey} = require("../config")
+const jwt =require("jsonwebtoken")
+function middleware(req,res,next)
+{
+try{
+  const ver =   jwt.verify(req.headers.authorization,jwtsecretkey);
+  if(ver.username)
+  {
+    next();
+  }
+  else
+  {
+    res.status(409).json({
+        msg : "connot verify user"
+    });
+  }
+}
+catch(e)
+{
+    res.status(409).json({
+        msg : "connot verify user"
+    });
+}
+}
